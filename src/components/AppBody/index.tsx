4740dc28c72f539dbc0ddb3834b7e489eb61f6a1
bonaxl015 <bonaxl015@gmail.com>
2025-03-10 18:47:03 +0800

    feat: body component

---
+++ src/components/AppBody/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC, ReactNode } from 'react';
+
+interface AppBodyProps {
+  children: ReactNode;
+}
+
+const AppBody: FC<AppBodyProps> = ({ children }) => {
+  return (
+    <div className="flex-1 flex flex-col container mx-auto p-4">{children}</div>
+  );
+};
+
+export default AppBody;
