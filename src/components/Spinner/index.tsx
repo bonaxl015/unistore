5526b96f78bc29f622d40540b5e147c7359442b0
bonaxl015 <bonaxl015@gmail.com>
2025-03-10 18:47:59 +0800

    feat: spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+const Spinner: FC = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="relative w-16 h-16">
+        <div className="absolute inset-0 border-4 border-transparent border-t-red-500 border-r-pink-500 rounded-full animate-spin"></div>
+        <div className="absolute inset-0 border-4 border-gray-700 rounded-full opacity-30"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
