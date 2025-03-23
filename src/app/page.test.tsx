8004320b17bbcdc0be7b1f0ead9a04cf6546181d
bonaxl015 <bonaxl015@gmail.com>
2025-03-23 12:36:23 +0800

    feat(homepage): home page development

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -3,6 +3,11 @@ import '@testing-library/jest-dom';
 
 import HomePage from './page';
 
+jest.mock('@/components/HomeCarouselSection', () => ({
+  __esModule: true,
+  default: () => <div data-testid="home-carousel-section"></div>
+}));
+
 describe('Given Home page', () => {
   it('should render normally', () => {
     const { getByLabelText } = render(<HomePage />);
